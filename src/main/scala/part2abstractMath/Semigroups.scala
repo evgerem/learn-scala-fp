c61ead2e8918ee2027261202a7e2046bf61b0cb0
Evgeny Eremeev <e.i.eremeev@gmail.com>
2022-12-27 00:55:49 +0200

    semigroups

---
+++ src/main/scala/part2abstractMath/Semigroups.scala
@@ -0,0 +1,33 @@
+package part2abstractMath
+
+// Specific imports:
+
+// import cats.YourTypeClass
+// import cats.instances.yourType._
+// import cats.syntax.yourTypeClass._
+
+// Everything
+
+// import cats._
+// import cats.implicits._
+
+object Semigroups {
+
+  // Semigroups combine elements of the same type
+
+  // Semigroup int and string
+
+  // Reduce and general API
+
+  // TODO 1: support a new type
+
+  // extension methods
+
+  // TODO 2: implement reduceThings2 with |+|
+
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
