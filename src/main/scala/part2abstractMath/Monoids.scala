10d6ec8640a7e98bccf86ed6ee1246dd6c0ab975
eeremeev <evgenii.eremeev@zalando.fi>
2023-01-01 14:06:03 +0200

    cw54

--- src/main/scala/part2abstractMath/Monoids.scala
+++ src/main/scala/part2abstractMath/Monoids.scala
@@ -4,15 +4,30 @@ import cats.Monoid
 
 
 object Monoids {
+  import cats.Semigroup
+  import cats.instances.int._
+  import cats.instances.string._
+  import cats.syntax.semigroup._
 
   // Semigroup limitation
+  val ints = (1 to 10).toList
+  val sumInts = ints.foldLeft(0)((a, b) => a |+| b)
 
   // Monoids
+  import cats.Monoid
+  val intMonoid = Monoid[Int]
+  val intResult = intMonoid.combine(44, 3)
+  val zero = intMonoid.empty
 
-  // extension method for monoid
+  // extension method for monoid |+|
 
   // TODO 1: implement a combine fold
-  def combineByFold[T](list: List[T])(implicit monoid: Monoid[T]): T = ???
+  def combineByFold[T](list: List[T])(implicit monoid: Monoid[T]): T = list.foldLeft(monoid.empty)(_ |+| _)
+  def combineByFold2[T](list: List[T])(implicit monoid: Monoid[T]): T = {
+    if (list.isEmpty) monoid.empty
+    else list.head |+| combineByFold2(list.tail)
+  }
+
 
   // TODO 2: combine a list of phonebooks as Map[String, Int]
   val phonebooks = List(
@@ -28,6 +43,7 @@ object Monoids {
       "Frank" -> 555
     )
   )
+  import cats.instances.map._
 
   // TODO 3
   case class ShoppingCart(items: List[String], total: Double)
@@ -35,7 +51,8 @@ object Monoids {
 
 
   def main(args: Array[String]): Unit = {
-
+    val strs = ints.map(_.toString)
+    println(combineByFold(phonebooks))
   }
 
 }
--- src/main/scala/part2abstractMath/Semigroups.scala
+++ src/main/scala/part2abstractMath/Semigroups.scala
@@ -14,22 +14,51 @@ package part2abstractMath
 object Semigroups {
 
   // Semigroups COMBINE elements of the same type
+  import cats.Semigroup
+  import cats.instances.int._
+  val intSemigroup = Semigroup[Int]
+  val intsCombined = intSemigroup.combine(4, 5)
+
+  import cats.instances.string._
+  val strSemigroup = Semigroup[String]
+  val stringsCombined = strSemigroup.combine("hello ", "world")
 
   // Semigroup int and string
 
   // Specific API
+  def reduceInts(list: List[Int]): Int = list.reduce(intSemigroup.combine)
+  def reduceStrings(list: List[String]): String = list.reduce(strSemigroup.combine)
 
   // General API
+  def reduceThings[T](list: List[T])(implicit semigroup: Semigroup[T]): T =
+    list.reduce(semigroup.combine)
 
   // TODO 1: support a new type
+  // hint: same as in Eq
+//  implicit val personInstance = Eq.instance[Person] { (a, b) =>
+//    a.name == b.name
+//  }
+
   case class Expense(id: Long, amount: Double)
+  implicit val expenseSemigroupInstance = Semigroup.instance[Expense] { (a, b) =>
+    Expense(id = a.id, amount = a.amount + b.amount)
+  }
 
   // extension methods
+  import cats.syntax.semigroup._
 
+  val intsSum = 1 |+| 2
+  val expenses = Expense(1, 10) |+| Expense(2, 25)
   // TODO 2: implement reduceThings2 with |+|
+  def reduceThings2[T](list: List[T])(implicit semigroup: Semigroup[T]): T =
+    list.reduce(_ |+| _)
 
   def main(args: Array[String]): Unit = {
+    val ints = (1 to 10).toList
+    val strs = ints.map(_.toString)
+    val expenses = ints.map(i => Expense(i, i * 1000))
 
+    println(reduceThings2(expenses))
   }
 
 }
