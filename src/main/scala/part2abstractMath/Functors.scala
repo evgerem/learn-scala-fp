91cd3665b74d6bc232b72eeeca93043e65e591f0
Evgeny Eremeev <e.i.eremeev@gmail.com>
2023-01-08 14:51:38 +0200

    functors: make Tree covariant

--- src/main/scala/part2abstractMath/Functors.scala
+++ src/main/scala/part2abstractMath/Functors.scala
@@ -16,9 +16,9 @@ object Functors {
 
   // TODO 1: define your own functor for a binary tree
   // hint: define an object which extends Functor[Tree]
-  trait Tree[T]
-  case class Leaf[T](value: T) extends Tree[T]
-  case class Branch[T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T]
+  trait Tree[+T]
+  case class Leaf[+T](value: T) extends Tree[T]
+  case class Branch[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T]
 
   // extension method - map
 
