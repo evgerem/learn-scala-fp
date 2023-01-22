d376df20577e317992b10b858cbf9e5691a7f2f9
Evgeny Eremeev <e.i.eremeev@gmail.com>
2023-01-22 22:25:38 +0200

    Monads

---
+++ src/main/scala/part2abstractMath/Monads.scala
@@ -0,0 +1,42 @@
+package part2abstractMath
+
+object Monads {
+
+  // lists
+
+  // TODO 1.1: how do you create all combinations of (number, char)?
+
+  // options
+
+  // TODO 1.2: how do you create the combination of (number, char)?
+
+  // futures
+
+  /*
+     Pattern
+   */
+
+  // trait MyMonad
+
+
+  // Cats Monad
+
+  // TODO 2: use a Monad[Future]
+
+  // specialized API
+
+  // generalize
+
+  // extension methods - weird imports - pure, flatMap
+
+  // TODO 3: implement the map method in MyMonad
+  // Monads extend Functors
+
+  // for-comprehensions
+
+  // TODO 4: implement a shorter version of getPairs using for-comprehensions
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
