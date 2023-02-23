ed66af4b7e8fe19043403d886a3a379b276a2ea1
Evgeny Eremeev <e.i.eremeev@gmail.com>
2023-02-23 23:11:06 +0200

    MonadTransformers

---
+++ src/main/scala/part2abstractMath/MonadTransformers.scala
@@ -0,0 +1,10 @@
+package part2abstractMath
+
+object MonadTransformers {
+
+  // TODO 1: Implement sumAllOptions
+  def sumAllOptions(values: List[Option[Int]]): Int =
+    ???
+
+  def main(args: Array[String]): Unit = {}
+}
